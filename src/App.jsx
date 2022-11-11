aaeb7b22ab35d6a87ec6f1a3ad90c1c338f25577
polukarp <perzhul.works@gmail.com>
2022-11-11 23:26:24 +0300

    fixed img issue

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,14 @@ function App() {
       title: "Wordle Clone",
       description:
         "Wordle - браузерная игра в слова, в которой пользователю предлагается угадать пятибуквенное английское слово за шесть попыток. Механика игры сделана с использованием связки React-Redux.",
-      image: "./src/assets/images/Screenshot 2022-11-10 at 23.50.11.png",
+      image: "Screenshot 2022-11-10 at 23.50.11.png",
       link: "https://wordle-clone-ten-psi.vercel.app/",
     },
     {
       title: "ToDo-приложение",
       description:
         "Простейшее ToDo-приложение с использованием библиотеки DaisyUI. ",
-      image: "src/assets/images/Screenshot 2022-11-11 at 14.27.06.png",
+      image: "Screenshot 2022-11-11 at 14.27.06.png",
       link: "https://todo-app-react-ruby-eight.vercel.app/",
     },
   ];
